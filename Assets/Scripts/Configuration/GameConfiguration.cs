051f99e6a0c8a58539cbb07aa55deb8403918dbc
Jason Tu <jasont@remarkablefoods.com>
2021-12-24 08:00:16 -0500

    add color shuffling

--- Assets/Scripts/Configuration/GameConfiguration.cs
+++ Assets/Scripts/Configuration/GameConfiguration.cs
@@ -18,19 +18,53 @@ public class GameConfiguration : ScriptableObject
     [Tooltip("Whether test mode is enabled. If enabled, the number of spot colors is restricted for ease of matching.")]
     public bool IsTestMode;
 
+    Color[] ShuffledColors;
+
+    void OnEnable()
+    {
+        ShuffledColors = Shuffle(Colors);
+    }
+
+    /// <summary>
+    /// Shuffle an array of colors and return the result.
+    /// </summary>
+    static Color[] Shuffle(Color[] colors)
+    {
+        var shuffledColors = new Color[colors.Length];
+
+        // Copy over colors first.
+        for (var i = 0; i < colors.Length; i++)
+        {
+            shuffledColors[i] = colors[i];
+		}
+
+        // Then shuffle the colors.
+        for (var i = colors.Length - 1; i >= 1; i--)
+        {
+            var j = Random.Range(0, i+1);
+
+            // Shuffle.
+            var temp = shuffledColors[i];
+            shuffledColors[i] = shuffledColors[j];
+            shuffledColors[j] = temp;
+		}
+
+        return shuffledColors;
+    }
+
     /// <summary>
     /// Choose a random color from GameConfiguration's set of possible colors.
     /// </summary>
     public Color RandomColor()
     {
-        var len = Colors.Length;
+        var len = ShuffledColors.Length;
         if (IsTestMode)
-        { 
-			// Temporarily choose fewer colors so it's easier to test matching spots.
-			len = 2;
+        {
+            // Temporarily choose fewer colors so it's easier to test matching spots.
+            len = 2;
 		}
 
         var i = Random.Range(0, len);
-        return Colors[i];
+        return ShuffledColors[i];
     }
 }
